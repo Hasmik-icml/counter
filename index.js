df4317e65f0e46dd672513e17fca4185fd543ec2
Hasmik-icml <gevorgyan.icml@gmail.com>
2022-02-14 23:48:18 +0400

    adding fibonaci numbers block

--- index.js
+++ index.js
@@ -3,36 +3,36 @@ const result = document.querySelector(".result");
 const buttonLower = document.querySelector(".buttonLower");
 const buttonAdd = document.querySelector(".buttonAdd");
 
-// buttonLower.addEventListener("click", function(){
-//     result.innerHTML--;
-// })
-// buttonAdd.addEventListener("click", function(){
-//     result.innerHTML++;
-// })
-
-
-document.querySelectorAll(".button").forEach(item => {
-    item.addEventListener("click", function(e){
-        console.log("Target",item.getAttribute("data-id"));
-        if (item.getAttribute("data-id") === "add"){
-            result.innerHTML++;
-        }else{
-            result.innerHTML--;
-        }
-    })
+buttonLower.addEventListener("click", function () {
+    result.innerHTML--;
 })
+buttonAdd.addEventListener("click", function () {
+    result.innerHTML++;
+})
+
+// second version
+// document.querySelectorAll(".button").forEach(item => {
+//     item.addEventListener("click", function(e){
+//         console.log("Target",item.getAttribute("data-id"));
+//         if (item.getAttribute("data-id") === "add"){
+//             result.innerHTML++;
+//         }else{
+//             result.innerHTML--;
+//         }
+//     })
+// })
 
 
 // calculate sum of two numbers 
-document.querySelector(".calculateNumber").addEventListener("click", function(){
+document.querySelector(".calculateNumber").addEventListener("click", function () {
     const number1 = +document.querySelector(".inputNumber1").value;
     const number2 = +document.querySelector(".inputNumber2").value;
     document.querySelector(".resultSum").innerHTML = calculate(number1)(number2);
 
 })
 
-function calculate(num1){
-    return function(num2){
+function calculate(num1) {
+    return function (num2) {
         return num1 + num2;
     }
 }
@@ -44,22 +44,32 @@ const buttonCansle = document.querySelector(".buttonCansle");
 const resultTimer = document.querySelector(".resultTimer");
 let interval;
 
-buttonStart.addEventListener("click", function(){
+buttonStart.addEventListener("click", function () {
     interval = setInterval(timerStart, 1000);
 })
 
-buttonStop.addEventListener("click", function(){
+buttonStop.addEventListener("click", function () {
     timerStop();
 })
 
-buttonCansle.addEventListener("click", function(){
+buttonCansle.addEventListener("click", function () {
     resultTimer.innerHTML = "0";
 })
 
-function timerStart(){
+function timerStart() {
     resultTimer.innerHTML++;
 }
 
-function timerStop(){
-  clearInterval(interval);
+function timerStop() {
+    clearInterval(interval);
 }
+
+//fibonachi
+let fibonachiNumbers = [0, 1];
+
+document.querySelector(".fibonachiButtons").addEventListener("click", function () {
+    let next = fibonachiNumbers.length;
+    fibonachiNumbers.push(fibonachiNumbers[fibonachiNumbers.length - 1] + fibonachiNumbers[fibonachiNumbers.length - 2]);
+    document.querySelector(".resultFibonachi").innerHTML += ", " + fibonachiNumbers[next];
+})
+
