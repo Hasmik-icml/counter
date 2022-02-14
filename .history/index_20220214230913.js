f775a20e15a4c5a00b72d10ce2a6761f6d625a51
Hasmik-icml <gevorgyan.icml@gmail.com>
2022-02-14 23:49:47 +0400

    adding fibonachi numbers block

---
+++ .history/index_20220214230913.js
@@ -0,0 +1,81 @@
+// increase decrease
+const result = document.querySelector(".result");
+const buttonLower = document.querySelector(".buttonLower");
+const buttonAdd = document.querySelector(".buttonAdd");
+
+buttonLower.addEventListener("click", function(){
+    result.innerHTML--;
+})
+buttonAdd.addEventListener("click", function(){
+    result.innerHTML++;
+})
+
+// second version
+// document.querySelectorAll(".button").forEach(item => {
+//     item.addEventListener("click", function(e){
+//         console.log("Target",item.getAttribute("data-id"));
+//         if (item.getAttribute("data-id") === "add"){
+//             result.innerHTML++;
+//         }else{
+//             result.innerHTML--;
+//         }
+//     })
+// })
+
+
+// calculate sum of two numbers 
+document.querySelector(".calculateNumber").addEventListener("click", function(){
+    const number1 = +document.querySelector(".inputNumber1").value;
+    const number2 = +document.querySelector(".inputNumber2").value;
+    document.querySelector(".resultSum").innerHTML = calculate(number1)(number2);
+
+})
+
+function calculate(num1){
+    return function(num2){
+        return num1 + num2;
+    }
+}
+
+// timer
+const buttonStart = document.querySelector(".buttonStart");
+const buttonStop = document.querySelector(".buttonStop");
+const buttonCansle = document.querySelector(".buttonCansle");
+const resultTimer = document.querySelector(".resultTimer");
+let interval;
+
+buttonStart.addEventListener("click", function(){
+    interval = setInterval(timerStart, 1000);
+})
+
+buttonStop.addEventListener("click", function(){
+    timerStop();
+})
+
+buttonCansle.addEventListener("click", function(){
+    resultTimer.innerHTML = "0";
+})
+
+function timerStart(){
+    resultTimer.innerHTML++;
+}
+
+function timerStop(){
+  clearInterval(interval);
+}
+
+//fibonachi
+document.queryCommandValue(".fibonachiButtons").addEventListener("click",function(){
+   let next = 1;
+   fib(next);
+})
+let fibonachiNumbers = [0, 1];
+
+function fib(next){
+    let nextFib; 
+    while(true){
+        nextFib = fibonachiNumbers[i] + fibonachiNumbers[i - 1];
+       next++;
+    }
+    return nextFib;
+}
---
+++ .history/index_20220214232930.js
@@ -0,0 +1,84 @@
+// increase decrease
+const result = document.querySelector(".result");
+const buttonLower = document.querySelector(".buttonLower");
+const buttonAdd = document.querySelector(".buttonAdd");
+
+buttonLower.addEventListener("click", function(){
+    result.innerHTML--;
+})
+buttonAdd.addEventListener("click", function(){
+    result.innerHTML++;
+})
+
+// second version
+// document.querySelectorAll(".button").forEach(item => {
+//     item.addEventListener("click", function(e){
+//         console.log("Target",item.getAttribute("data-id"));
+//         if (item.getAttribute("data-id") === "add"){
+//             result.innerHTML++;
+//         }else{
+//             result.innerHTML--;
+//         }
+//     })
+// })
+
+
+// calculate sum of two numbers 
+document.querySelector(".calculateNumber").addEventListener("click", function(){
+    const number1 = +document.querySelector(".inputNumber1").value;
+    const number2 = +document.querySelector(".inputNumber2").value;
+    document.querySelector(".resultSum").innerHTML = calculate(number1)(number2);
+
+})
+
+function calculate(num1){
+    return function(num2){
+        return num1 + num2;
+    }
+}
+
+// timer
+const buttonStart = document.querySelector(".buttonStart");
+const buttonStop = document.querySelector(".buttonStop");
+const buttonCansle = document.querySelector(".buttonCansle");
+const resultTimer = document.querySelector(".resultTimer");
+let interval;
+
+buttonStart.addEventListener("click", function(){
+    interval = setInterval(timerStart, 1000);
+})
+
+buttonStop.addEventListener("click", function(){
+    timerStop();
+})
+
+buttonCansle.addEventListener("click", function(){
+    resultTimer.innerHTML = "0";
+})
+
+function timerStart(){
+    resultTimer.innerHTML++;
+}
+
+function timerStop(){
+  clearInterval(interval);
+}
+
+//fibonachi
+let fibonachiNumbers = [0, 1];
+
+document.querySelector(".fibonachiButtons").addEventListener("click",function(){
+   let next = fibonachiNumbers.length + 1;
+  let res =  fib(next);
+  console.log(res);
+  console.log(next);
+  document.querySelector(".resultFibonachi").innerHTML = res[next];
+  
+})
+
+
+function fib(next){
+    console.log(next)
+    fibonachiNumbers.push(fibonachiNumbers[fibonachiNumbers.length] + fibonachiNumbers[fibonachiNumbers.length - 1]);
+    console.log(fibonachiNumbers);
+}
